44c323f85cf24aae1f686ab8bad803f146f10bb3
Mija Azdajic <63316773+mijaazdajic@users.noreply.github.com>
2021-03-09 09:48:05 -0500

    Update 04_Merging_Rate_Chem.R

--- 04_Merging_Rate_Chem.R
+++ 04_Merging_Rate_Chem.R
@@ -1,5 +1,5 @@
 ###############################################################################################
-#####THIS R SCRIPT IS FOR MERGING METHYLATION AND CHEM DATA TO ANALYSE#########################
+#####THIS R SCRIPT IS FOR MERGING METHYLATION AND CHEM DATA FOR STATS##########################
 #####We will use data made in the last three scripts and a csv file with chem data#############
 ###############################################################################################
 
@@ -13,19 +13,26 @@ library(ggpubr) #probably don't need it
 library(car) #for stats stuff like crPLots
 library(lmtest) #for assumptions of reg
 library(MASS) #for stepAIC
-library(lmtest)
+library(lmtest) #residual testing
 library(ggiraphExtra)
 
 
 #####make a theme###########
-theme_incubation <- theme(panel.background = element_rect(fill = "white", linetype = "solid", colour = "black"), 
-                          legend.key = element_rect(fill = "white"), panel.grid.minor = element_blank(), panel.grid.major = element_blank(), 
-                          axis.text=element_text(size=17),                                               #axis numbers
-                          plot.title=element_text(size=30, hjust=-0.35),                                 #Main title      --> to center title, hjust=0.5, to left align -0.2
-                          legend.title = element_text(size = 16, colour = "black", angle = 0),           #Legend title
-                          legend.text = element_text(size = 14, colour = "black", angle = 0),            #Legend text
-                          strip.text.x = element_text(size = 20, colour = "black", angle = 0, vjust = 1),        #Facet x text size
-                          strip.text.y = element_text(size = 20, colour = "black", angle = 270),
+theme_incubation <- theme(panel.background = element_rect(fill = "white", 
+                                                          linetype = "solid", colour = "black"), 
+                          legend.key = element_rect(fill = "white"), 
+                          panel.grid.minor = element_blank(), 
+                          panel.grid.major = element_blank(), 
+                          axis.text=element_text(size=17),                          #axis numbers
+                          plot.title=element_text(size=30, hjust=-0.35),            #Main title      --> to center title, hjust=0.5, to left align -0.2
+                          legend.title = element_text(size = 16, 
+                                                      colour = "black", angle = 0),           #Legend title
+                          legend.text = element_text(size = 14, 
+                                                     colour = "black", angle = 0),            #Legend text
+                          strip.text.x = element_text(size = 20, 
+                                                      colour = "black", angle = 0, vjust = 1),  #Facet x text size
+                          strip.text.y = element_text(size = 20, 
+                                                      colour = "black", angle = 270),
                           strip.background = element_blank(),
                           panel.border = element_blank())   
 
@@ -34,19 +41,20 @@ theme_incubation <- theme(panel.background = element_rect(fill = "white", linety
 # Make a mastersheet with chem and methylation ----------------------------
 
 
-water_chem <- read_csv("CHEM_DATA/COMPLETE_MASTER_SHEET_ALL_WATER_CHEM.csv",local = locale(encoding = "latin1"))
+water_chem <- read_csv("CHEM_DATA/COMPLETE_MASTER_SHEET_ALL_WATER_CHEM.csv",
+                       local = locale(encoding = "latin1")) #data sheet contains "lake_name" as first column and chem data as subsequent columns
 
-suva <- read_csv("CHEM_DATA/SUVA_Analysis.csv", local = locale(encoding = "latin1"))
+suva <- read_csv("CHEM_DATA/SUVA_Analysis.csv", local = locale(encoding = "latin1")) #add SUVA data from seperate analysis
 
-years <- read_csv("INFO_SHEETS/Sampling_Year_Per_Lake.csv", locale = locale(encoding = "latin1"))
+years <- read_csv("INFO_SHEETS/Sampling_Year_Per_Lake.csv", locale = locale(encoding = "latin1")) #info sheet with sampling year for each lake
 
-methylation_table <- read_csv("methylation_table.csv", local = locale(encoding = "latin1"))
+methylation_table <- read_csv("methylation_table.csv", local = locale(encoding = "latin1")) #dataframe made in previous script
 
-methylation_table <- methylation_table %>% merge(.,years, by = "lake_name", all=T)
+methylation_table <- methylation_table %>% merge(.,years, by = "lake_name", all=T) #add the years to methylation table
 
 mastersheet <- left_join(water_chem, methylation_table, by = c("lake_name","Year")) %>% 
                 left_join(., suva, by = "lake_name", all=T) %>% mutate(., proton_M = 10^(-pH)) %>% 
-                filter(., !lake_name %in% c("VEE"))
+                filter(., !lake_name %in% c("VEE")) #merge all methylation and chem data together, take out VEE because the incubation experiment was not succesful
 
 #write.csv(mastersheet, file = "SHEETS_FROM_EACH_SCRIPT/04_mastersheet.csv")
 
@@ -63,12 +71,12 @@ subset_1 <- subset(mastersheet, select = c("lake_name", "Percent_Hg", "Nitrogen_
 
 # I think a stepwise regression with Km and other varaibles would  --------
 
-#start with scatterplot matrix
-scatterplotMatrix(~log(km_24) + log(Sulphate+1) + log(SUVA_percent) + log(Total_Arsenic_UO) + 
+
+scatterplotMatrix(~log(km_24) + log(Sulphate+1) + log(SUVA_percent) + log(Total_Arsenic_UO) + #start with scatterplot matrix
                     log(proton_M) + log(DOC) + log(Iron_Total) + 
                     Total_Phophorus_UO + log(Percent_Hg), reg.line = lm,
                   smooth = TRUE, span = 0.5,
-                  diagonal = "denisty", data = subset_1) #decided to make sure they were normally distributed before anlaysis
+                  diagonal = "denisty", data = subset_1) #decided to make sure that raw data was normally distributed before anlaysis
 
 subset_1 <- subset_1 %>% mutate(., logkm = log(km_24))
 
@@ -77,8 +85,8 @@ full.model <- lm(logkm ~ log(Sulphate+1) + log(Percent_Hg) + SUVA_percent +
                    Total_Phophorus_UO, data = subset_1)
 step.model <- stepAIC(full.model, direction = "both") #DOC 
 summary(step.model)
-bptest(step.model,varformula = ~fitted.values(step.model), studentize=T, data=subset_1)
-resettest(step.model, power = 2:3, type = "regressor", data = subset_1)
+bptest(step.model,varformula = ~fitted.values(step.model), studentize=T, data=subset_1) #passed
+resettest(step.model, power = 2:3, type = "regressor", data = subset_1) #passed
 shapiro.test(step.model$residuals) #passed
 
 par(mfrow=c(1,2))
@@ -90,59 +98,7 @@ lines(xseq,dnorm(xseq,sd=summary(step.model)$sigma))
 
 ########DOC is the best predictor of pooled km_24###############
 
-
-# Stepwise regression with Kd_24 as dependent -----------------------------
-
-# I think a stepwise regression with Kd and other varaibles would  --------
-scatterplotMatrix(~logkd + log(Sulphate+1) + SUVA_percent + log(Total_Arsenic_UO) + 
-                    log(proton_M) + log(DOC) + log(Iron_Total) + 
-                    log(Total_Phophorus_UO+1), reg.line = lm,
-                  smooth = TRUE, span = 0.5,
-                  diagonal = "denisty", data = subset_1) #decided to make sure they were normally distributed before anlaysis
-
-
-#best normalize
-x <- rgamma(29, 1, 1)
-
-yeojohnson_obj <- yeojohnson(subset_1$kd_24)
-yeojohnson_obj
-p <- predict(yeojohnson_obj)
-x2 <- predict(yeojohnson_obj, newdata = p, inverse = TRUE)
-
-all.equal(x2, x)
-
-
-
-boxCox(logkd, family="yjPower", plotit = TRUE)
-
-
-full.model <- lm(logkd ~ log(Sulphate+1) + SUVA_percent + log(Total_Arsenic_UO) + 
-                   pH + log(DOC) + log(Iron_Total) + 
-                   log(Total_Phophorus_UO+1), data = subset_1)
-step.model <- stepAIC(full.model, direction = "both") #none
-summary(step.model)
-bptest(step.model,varformula = ~fitted.values(step.model), studentize=T, data=subset_1)
-resettest(step.model, power = 2:3, type = "regressor", data = subset_1)
-shapiro.test(step.model$residuals) #did not pass = p=0.03, not great
-
-
-#make a more simple model with only arsenic
-model.doc <- lm(logkd ~ log(Total_Arsenic_UO), data = subset_1)
-summary(model.doc)
-bptest(model.doc,varformula = ~fitted.values(model.doc), studentize=T, data=subset_1)#passed
-resettest(model.doc, power = 2:3, type = "regressor", data = subset_1)#passed
-shapiro.test(model.doc$residuals) #passed
-
-#make more simple model with only sulphate
-model.sul <- lm(logkd ~ log(Sulphate+1), data = subset_1)
-summary(model.sul)
-bptest(model.sul,varformula = ~fitted.values(model.sul), studentize=T, data=subset_1)#passed
-resettest(model.sul, power = 2:3, type = "regressor", data = subset_1)#passed
-shapiro.test(model.sul$residuals) #passed
-
-
 # Show all the variables as dependent with Km_24 --------------------------
-
 
 sul <- ggplot(data = mastersheet, aes(x=Sulphate, y=km_24, label = lake_name)) + 
   geom_point(aes(size=4), show.legend = FALSE) +
@@ -198,7 +154,7 @@ ggsave(allgraph, file = "methylation_rate_with_varaibles.svg", dpi = 600, path =
 
 
 # Show all the variables as dependent with Kd_24 --------------------------
-
+# stats for Kd is in other script -----------------------------------------
 
 sul <- ggplot(data = mastersheet, aes(x=Sulphate, y=kd_24)) + 
   geom_point(aes(size=1), show.legend = FALSE) +
@@ -462,8 +418,6 @@ bptest(model.sul,varformula = ~fitted.values(model.sul), studentize=T, data=high
 resettest(model.sul, power = 2:3, type = "regressor", data = high.km)
 shapiro.test(model.sul$residuals) #passed
 
-makeEq(model.sul, digits = 2)
-
 anova(step.model, model.sul) #there is no significant difference between the two models, will choose the more simple one
 
 crPlots(model.sul)
@@ -478,95 +432,5 @@ sul
 model <- ggPredict(model.sul, interactive = T) +
   labs(x = "Sulphate (mg/L)", y = 'Methylation Rate Constant (h^-1)')
 
-
-
-
-# Make a figure with DOC/Km and Km/Sulfate with different colours ---------
-
-doc <- ggplot(data = mastersheet, aes(x=(DOC), y=(km_24))) + 
-  labs(x = "DOC (mg/L)", y = 'Methylation Rate Constant (h^-1)') +
-  geom_smooth(formula = 'y ~ log(x)', method = 'glm', 
-              method.args = list(family = gaussian(link = 'log')), 
-              fill = "#515c69", color = "#515c69") +
-  geom_point(aes(size=4), show.legend = FALSE) +
-  xlim (0, 70) +
-  annotate('text', x =67, y = 0.1, label = "p-value==0.016~~R^{2}==0.17" ,parse = TRUE,size=4) +
-  theme_incubation
-
-doc
-
-
-
-sul <- ggplot(data = joined.mastersheet, aes(x=Sulphate, y=km_24, label = lake_name, color = Group)) + 
-  geom_point(size=4, show.legend = T) +
-  #geom_text_repel() +
-  labs(x = "Sulphate (mg/L)", y = 'Methylation Rate Constant (h^-1)') +
-  scale_fill_brewer()+
-  theme_incubation +
-  theme(legend.position = c(0.9,0.8))
-
-
-sul
-
-
-figure_4 <- ggarrange(doc, sul, ncol = 1, nrow = 2,
-                      labels = c("A", "B"),
-                      font.label = list(size = 25),
-                      hjust = 0, vjust = 2,
-                      label.x = 0.07,
-                      align="hv")
-
-figure_4
-
-
-ggsave("Figure_4.svg", plot=figure_4, width = 45, height = 30, units = c("cm"), path = "Figures/")
-
-
-
-###############################################################################################
-###############################################################################################
-###############################################################################################
-###############################################################################################
-
-
-
-# calculate the ratio of km/kd and find predictors ------------------------
-
-
-# make new dataframe with km/kd ratio -------------------------------------
-
-# only use the data points that have demethylation ------------------------
-
-
-
-mastersheet.ratio <- mastersheet %>% dplyr::filter(., kd_24 > 0) %>% 
-  mutate(., ratio = kd_24/km_24)
-
-# Stepwise regression with Km_24 as dependent -----------------------------
-
-#start with scatterplot matrix
-scatterplotMatrix(~log(ratio) + log(Sulphate+1) + log(SUVA_percent) + sqrt(Total_Arsenic_UO) + sqrt(pH) + sqrt(DOC) + sqrt(Iron_Total) + 
-                    sqrt(Total_Phophorus_UO) + log(Percent_Hg), reg.line = lm,
-                  smooth = TRUE, span = 0.5,
-                  diagonal = "denisty", data = mastersheet.ratio) #decided to make sure they were normally distributed before anlaysis
-
-
-subset.ratio <- mastersheet.ratio %>% mutate(., logratio = log(ratio))
-
-full.model <- lm(logratio ~ log(Sulphate+1) + log(Percent_Hg) + SUVA_percent + log(Total_Arsenic_UO) + pH + log(DOC) + 
-                   Total_Phophorus_UO, data = subset.ratio) #took off total iron, do not have enough DFs to run all the independent variables
-step.model <- stepAIC(full.model, direction = "both") #DOC
-summary(step.model)
-bptest(step.model,varformula = ~fitted.values(step.model), studentize=T, data=subset.ratio)
-resettest(step.model, power = 2:3, type = "regressor", data = subset.ratio)
-shapiro.test(step.model$residuals) #passed
-
-
-sul <- ggplot(data = subset.ratio, aes(x=log(Sulphate+1), y=logratio, label = lake_name)) +
-  geom_point(aes(size=1), show.legend = FALSE) +
-  labs(x = "log(Sulphate + 1 ) (mg/L)", y = 'Methylation Rate Constant (h^-1)') +
-  stat_smooth(method = 'lm', formula = y ~ x, se = T, aes(colour = "darkgrey")) +
-  theme_incubation
-sul
 
 
