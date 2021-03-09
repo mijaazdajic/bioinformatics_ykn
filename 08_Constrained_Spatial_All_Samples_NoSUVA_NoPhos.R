0e277c0ed9e2a08bf11e2634dde706c6a0d5c01a
Mija Azdajic <63316773+mijaazdajic@users.noreply.github.com>
2021-03-09 11:45:34 -0500

    Update and rename 11_Constrained_Spatial_All_Samples_NoSUVA_NoPhos.R to 08_Constrained_Spatial_All_Samples_NoSUVA_NoPhos.R

--- 08_Constrained_Spatial_All_Samples_NoSUVA_NoPhos.R
+++ 08_Constrained_Spatial_All_Samples_NoSUVA_NoPhos.R
@@ -11,7 +11,24 @@ library(phyloseq)
 library(vegan)
 library(dplyr)
 library(ape)
+library(ggthemes)
+library(ggpubr)
+library(ggrepel)
 
+
+#####make a theme###########
+theme_incubation <- theme(panel.background = element_rect(fill = "white", linetype = "solid", colour = "black"), 
+                          legend.key = element_rect(fill = "white"), panel.grid.minor = element_blank(), panel.grid.major = element_blank(), 
+                          axis.title = element_text(size = 20),
+                          axis.text=element_text(size=40),                                               #axis numbers
+                          plot.title=element_text(size=30, hjust=-0.35),                                 #Main title      --> to center title, hjust=0.5, to left align -0.2
+                          legend.title = element_text(size = 16, colour = "black", angle = 0),           #Legend title
+                          legend.text = element_text(size = 14, colour = "black", angle = 0),            #Legend text
+                          strip.text.x = element_text(size = 50, colour = "black", angle = 0, vjust = 1),        #Facet x text size
+                          strip.text.y = element_text(size = 50, colour = "black", angle = 270),
+                          strip.background = element_blank(),
+                          panel.border = element_blank(),
+                          plot.margin = unit(c(1,4,1,1), "lines"))
 
 
 # load data from previous scripts -----------------------------------------
@@ -29,12 +46,14 @@ methylation_table <- methylation_table %>% merge(.,years, by = "lake_name", all=
 methylation_table$lake_name <- gsub("NIVEN", "NIVEN_2016", methylation_table$lake_name)
 methylation_table$lake_name <- gsub("BC20", "BC20_2016", methylation_table$lake_name)
 
+
 env.data <- left_join(metadata, methylation_table, by = c("lake_name","Year")) %>% 
   left_join(., suva, by = "lake_name", all=T) %>% 
   dplyr::select(.,c('#SampleID', lake_name, MeHg_water_ngl, Year, THg_water_ngl, Percent_Hg, DOC, pH,
                     Sulphate, Iron_Total, Dissolved_Phosphorus_UO, Total_Phophorus_UO, 
                     Dissolved_Arsenic_UO, Total_Arsenic_UO, km_24, kd_24, SUVA, SUVA_percent)) %>% 
-  dplyr::filter(., !lake_name %in% c("NIVEN_2015", "BC20_2015"))
+  subset(., lake_name != "POCKET") %>% 
+  filter(., !lake_name %in% c("NIVEN_2015", "BC20_2015"))
 
 
 
@@ -56,10 +75,17 @@ physeq2<-phyloseq(
 
 physeq2
 
+
+
+# remove Pocket -----------------------------------------------------------
+
+physeq2 = subset_samples(physeq2, lake_name != "POCKET")
+
+
+
 #Remove taxa not seen more than 3 times in at least 50% of the samples. This protects 
 #against an OTU with small mean & trivially large C.V.
 GP1 = filter_taxa(physeq2, function(x) sum(x > 3) > (0.5*length(x)), TRUE)
-
 GP1 = subset_samples(GP1, lake_name != "BC20_2015") 
 GP1 = subset_samples(GP1, lake_name != "NIVEN_2015")
 
@@ -85,10 +111,10 @@ p
 # Load spatial data -------------------------------------------------------
 
 space <- read_csv("INFO_SHEETS/Spatial_Coordinates.csv", local = locale(encoding = "latin1"))  %>% 
-  left_join(., env.data, by = c("lake_name", "Year")) %>% dplyr::select(., c(lake_name, Latitude, Longitude, `#SampleID`)) %>% 
-  na.omit(.) %>% filter(., !lake_name %in% c("NIVEN_2015", "BC20_2015")) %>% 
+  left_join(., env.data, by = c("lake_name", "Year")) %>% 
+  dplyr::select(., c(lake_name, Latitude, Longitude, `#SampleID`)) %>% 
+  na.omit(.) %>% filter(., !lake_name %in% c("NIVEN_2015", "BC20_2015", "POCKET")) %>% 
   column_to_rownames("#SampleID") %>% dplyr::select(-c(lake_name))
-  
   
 
 # PCNM calculations -------------------------------------------------------
@@ -100,10 +126,11 @@ pcnm <- as.matrix(pcnm$dist)
 
 space.ord <- prcomp(pcnm, scale = T)
 eigs <-  space.ord$sdev^2
-eigs[1] / sum(eigs) #first eigen vector explains 0.9726
-eigs[2] / sum(eigs) #second eigen vector explains 0.0185
+eigs[1] / sum(eigs) #first eigen vector explains 0.5654
+eigs[2] / sum(eigs) #second eigen vector explains 0.1306
 
-PCspace <- as.data.frame(space.ord$rotation[,1]) #pick out only the first vector since it explains ~100% of variability
+
+PCspace <- as.data.frame(space.ord$rotation[,1]) #pick out only the first vector since it explains >50% of variability
 
 
 
@@ -114,7 +141,7 @@ env.data <-  env.data %>% column_to_rownames(., var = "#SampleID") %>%
   merge(., PCspace, by = "row.names", all=T) %>% 
   dplyr::select(-"lake_name") %>% dplyr::select(-"Year") %>%  #take out row names that are not num so can be standardized
   column_to_rownames(var = "Row.names") %>%
-  rename(space.vector= 'space.ord$rotation[, 1]')
+  dplyr::rename(space.vector= 'space.ord$rotation[, 1]')
 
 # Transform variables to have normal distribution -------------------------
 
@@ -131,16 +158,15 @@ hist(sqrt(env.data$Total_Phophorus_UO)) #better
 env.data$sqrtP <- sqrt(env.data$Total_Phophorus_UO) 
 hist(env.data$km_24) #looks ok
 hist(env.data$space.vector)
-hist((env.data$space.vector)^(-1)) #better
-env.data$space.trans <- 1/(env.data$space.vector)
+hist(log10(env.data$space.vector+1)) #better
+env.data$space.trans <- log10(env.data$space.vector+1)
 hist(env.data$SUVA) #good
 
 # Scale Environmental variables -------------------------------------------
 
 scaled.env <-  env.data %>% 
   decostand(., method = "standardize", na.rm = T) %>% 
-  select("space.trans", "DOC", "pH", "lnsul", "lnFe") %>% 
-  na.omit()
+  dplyr::select("space.trans", "SUVA","DOC", "pH", "lnsul", "lnFe", "sqrtP")
 
 # Make a distance matrix using UNIFRAC weighted ---------------------------
 
@@ -150,8 +176,10 @@ wUF.table<-as.matrix(dist(wUF.dm))
 
 # Calculate distance based RDA using Weighted UniFrac distance mat --------
 
-dbRDA<-capscale(wUF.dm ~ space.trans + DOC + pH + lnsul +
-                  + lnFe, data=scaled.env, sqrt.dist = T)
+dbRDA<-capscale(wUF.dm ~ space.trans * DOC * pH * lnsul *
+                   lnFe * SUVA * sqrtP, data=scaled.env, sqrt.dist = T)
+
+
 
 plot(dbRDA, main = "dbRDA")
 summary(dbRDA)
@@ -166,9 +194,30 @@ vif.cca(dbRDA) # a vif of more than 10 indicates that a variable is strongly dep
 rda.1 <- capscale(wUF.dm~1, data = scaled.env, sqrt.dist = T)
 mod <- ordistep(rda.1, scope = formula(dbRDA),
                 direction = "both", permutations = 999, trace = T, sqrt.dist = T) #bidirectional selection
-
 summary(mod)
-anova(mod)
+anova(mod, permutations = 999) #the overall model is significant
+anova(mod, by = "terms", permutations = 999) #space is not so significant, might slighly change since permuations
+anova(mod, by = "axis", permutations = 999) #the CAP3 is not significant
+
+mod2<-capscale(wUF.dm ~ DOC + SUVA + space.trans, data=scaled.env, sqrt.dist = T)
+anova(mod2, permutations = 999) #better p-value
+anova(mod2, by = "terms", permutations = 999) #DOC and SUVA is significant
+anova(mod2, by = "axis", permutations = 999) #CAP1 and CAP2 is significant
+
+
+
+summary(mod2)
+
+#check effect of variables
+doc <- capscale(wUF.dm ~ Condition(SUVA) + DOC + Condition(space.trans), data=scaled.env, sqrt.dist = T)
+summary(doc) #6.84%
+
+suva <- capscale(wUF.dm ~ Condition(DOC) + SUVA + Condition(space.trans), data=scaled.env, sqrt.dist = T)
+summary(suva) #6.72%
+
+space <- capscale(wUF.dm ~ Condition(DOC) + Condition(SUVA) + space.trans, data=scaled.env, sqrt.dist = T)
+summary(space) #5.86%
+
 
 
 plot(mod)
@@ -176,62 +225,53 @@ summary(mod)
 screeplot(mod)
 RsquareAdj(mod)
 
-# Make plot ---------------------------------------------------------------
-
-summm <- summary(mod)
-plot(mod)
-xtext <- paste("(", round(summm$concont[[1]][2, 1] * 100, 2), " % of fitted, ", 
-               round(summm$cont[[1]][2, 1] * 100, 2), " % of total variation)", sep = "")
-ytext <- paste("(", round(summm$concont[[1]][2, 2] * 100, 2), " % of fitted, ", 
-               round(summm$cont[[1]][2, 2] * 100, 2), " % of total variation)", sep = "")
-mtext(xtext, 1, line = 4)
-mtext(ytext, 2, line = 2)
-
-
-
-# Do the same with Unweighted UNIFRAC -------------------------------------
-# Make a distance matrix using UNIFRAC unweighted ---------------------------
-
-uwUF.dm<-UniFrac(GP2, weighted = FALSE, fast = TRUE) 
-uwUF.table<-as.matrix(dist(uwUF.dm))
-
-
-# Calculate distance based RDA using Weighted UniFrac distance mat --------
-
-dbRDA<-capscale(uwUF.dm ~ space.trans + DOC + pH + lnsul +
-                  + lnFe, data=scaled.env, sqrt.dist = T)
-
-plot(dbRDA, main = "dbRDA")
-summary(dbRDA)
-screeplot(dbRDA)
-RsquareAdj(dbRDA)##Variance partition
-
-
-vegan::anova.cca(dbRDA, by="term", step = 200)
-vif.cca(dbRDA) # a vif of more than 10 indicates that a variable is strongly dependent on others
-
-# Do a stepwise regression to find variables that best explain species differeces
-rda.1 <- capscale(uwUF.dm~1, data = scaled.env, sqrt.dist = T)
-mod <- ordistep(rda.1, scope = formula(dbRDA),
-                direction = "both", permutations = 999, trace = T, sqrt.dist = T) #bidirectional selection
-
-mod
-summary(mod)
-anova(mod)
-
-
-plot(mod)
-summary(mod)
-screeplot(mod)
-RsquareAdj(mod)
 
 # Make plot ---------------------------------------------------------------
 
-summm <- summary(mod)
-plot(mod)
-xtext <- paste("(", round(summm$concont[[1]][2, 1] * 100, 2), " % of fitted, ", 
-               round(summm$cont[[1]][2, 1] * 100, 2), " % of total variation)", sep = "")
-ytext <- paste("(",
-               round(summm$cont[[1]][2, 2] * 100, 2), " % of total variation)", sep = "")
-mtext(xtext, 1, line = 4)
-mtext(ytext, 2, line = 2)
+smry <- summary(mod2)
+scrs <- scores(mod2)
+df1  <- data.frame(smry$sites[,1:2]) # site scores for RDA1 and RDA2
+df1$site <- rownames(df1)  #add site names
+df2  <- data.frame(smry$biplot[,1:2])  # mapping environmental variables
+
+name_key <- read_csv("INFO_SHEETS/lake_name_key.csv", local = locale(encoding = "latin1"))
+df1 <- df1 %>% left_join(., name_key, by = "site") %>% column_to_rownames(., var = "site")
+
+
+
+xtext <- paste("CAP 1 (", round(smry$concont[[1]][2, 1] * 100, 2), " % of fitted, ", 
+               round(smry$cont[[1]][2, 1] * 100, 2), " % of total variation)", sep = "")
+ytext <- paste("CAP 2 (", round(smry$concont[[1]][2, 2] * 100, 2), " % of fitted, ", 
+               round(smry$cont[[1]][2, 2] * 100, 2), " % of total variation)", sep = "")
+
+
+
+rda.plot <- ggplot(df1, aes(x=CAP1, y=CAP2, colour = lake_name)) + 
+  geom_hline(yintercept=0, linetype="dotted") +
+  geom_vline(xintercept=0, linetype="dotted") +
+  geom_point(size = 4) +
+  geom_text_repel(aes(label=lake_name), colour = "grey50",size=5, point.padding = 0.5, 
+                  segment.color = 'transparent') +
+  geom_segment(data=df2, aes(x=0, xend=CAP1, y=0, yend=CAP2), 
+               color="grey25", arrow=arrow(length=unit(0.01,"npc"))) +
+  geom_text(data=df2, aes(x=CAP1,y=CAP2,label=rownames(df2),
+                          hjust=0.5*(1-sign(CAP1)),vjust=0.5*(1-sign(CAP2))), 
+            color="grey25", size=6) +
+  #xlim(-1.05, 1.65) +
+  #ylim(-1.5, 1.05) +
+  xlab(xtext) +
+  ylab(ytext) + 
+  coord_fixed() +
+  theme_incubation +
+  theme(legend.position = "right",
+        legend.title = element_blank())
+
+
+rda.plot
+
+figure_5 <- rda.plot
+
+ggsave(figure_5, file="Figure_5.svg", width = 10, height=10, units = "in", path = "Figures/")
+
+
+########================================================== DONE ================================#################
